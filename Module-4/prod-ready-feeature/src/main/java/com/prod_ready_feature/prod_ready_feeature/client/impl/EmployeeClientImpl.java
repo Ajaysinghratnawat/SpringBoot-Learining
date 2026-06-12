485e9dc20ae1f9ff9e0853e4f1a48c1dda528c88
Ajaysinghratnawat <ajaysinghratnawat113@email.com>
2026-06-12 11:19:58 +0530

    Logger/Logging

--- Module-4/prod-ready-feeature/src/main/java/com/prod_ready_feature/prod_ready_feeature/client/impl/EmployeeClientImpl.java
+++ Module-4/prod-ready-feeature/src/main/java/com/prod_ready_feature/prod_ready_feeature/client/impl/EmployeeClientImpl.java
@@ -4,6 +4,8 @@ import com.prod_ready_feature.prod_ready_feeature.advice.ApiResponse;
 import com.prod_ready_feature.prod_ready_feeature.client.EmployeeClient;
 import com.prod_ready_feature.prod_ready_feeature.dto.EmployeeDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -15,8 +17,15 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+        log.error("error log");
+        log.warn("warn log");
+        log.info("info log");
+        log.debug("debug log");
+        log.trace("trace log");
         try{
             ApiResponse<List<EmployeeDTO>> employeeDTOList = restClient.get().uri("employees").retrieve().body(new ParameterizedTypeReference<>() {});
             return employeeDTOList.getData();
