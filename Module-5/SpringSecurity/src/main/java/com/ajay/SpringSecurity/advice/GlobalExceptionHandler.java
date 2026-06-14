4c4300a7500fb693dc3c11e3975e2448ffc62edd
Ajaysinghratnawat <ajaysinghratnawat113@email.com>
2026-06-14 20:21:33 +0530

    Spring security exception handling

--- Module-5/SpringSecurity/src/main/java/com/ajay/SpringSecurity/advice/GlobalExceptionHandler.java
+++ Module-5/SpringSecurity/src/main/java/com/ajay/SpringSecurity/advice/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ajay.SpringSecurity.advice;
 
 import com.ajay.SpringSecurity.exceptions.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
@@ -16,16 +17,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 
-//    @ExceptionHandler(AuthenticationException.class)
-//    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException ex) {
-//        ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.UNAUTHORIZED);
-//        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
-//    }
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException ex) {
+        ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
 
-//    @ExceptionHandler(JwtException.class)
-//    public ResponseEntity<ApiError> handleJwtException(JwtException ex) {
-//        ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.UNAUTHORIZED);
-//        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
-//    }
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException ex) {
+        ApiError apiError = new ApiError(ex.getLocalizedMessage(), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
 
 }
