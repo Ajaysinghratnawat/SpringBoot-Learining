48a7ad56140417dc1ac703e4613f9de4900e88da
Ajaysinghratnawat <ajaysinghratnawat113@email.com>
2026-06-16 17:40:26 +0530

    User Session management with JWT

--- Module-5,6/SpringSecurity/src/main/java/com/ajay/SpringSecurity/services/AuthService.java
+++ Module-5,6/SpringSecurity/src/main/java/com/ajay/SpringSecurity/services/AuthService.java
@@ -16,6 +16,7 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final UserService userService;
+    private final SessionService sessionService;
 
     public LoginResponseDto login(LoginDto loginDto) {
         Authentication authentication = authenticationManager.authenticate(
@@ -24,13 +25,15 @@ public class AuthService {
         User user = (User) authentication.getPrincipal();
         String accesstoken = jwtService.generateAccessToken(user);
         String refreshToken = jwtService.generateRefreshToken(user);
-        System.out.println(accesstoken);
-        System.out.println(refreshToken);
+        sessionService.generateNewSession(user,refreshToken);
+//        System.out.println(accesstoken);
+//        System.out.println(refreshToken);
         return new LoginResponseDto(user.getId(),accesstoken,refreshToken);
     }
 
     public LoginResponseDto refreshToken(String refreshToken) {
         Long userId = jwtService.getUserIdFromToken(refreshToken);
+        sessionService.validateSession(refreshToken);
         User user = userService.getUserById(userId);
         String accesstoken = jwtService.generateAccessToken(user);
         return new LoginResponseDto(user.getId(),accesstoken,refreshToken);
