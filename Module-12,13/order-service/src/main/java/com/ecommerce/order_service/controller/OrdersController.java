8d6a15789c7fc7dbaf3c1e8d6a0b5a9547d7d47e
ajay ratnawat <ajay.ratnawat@synoriq.in>
2026-07-07 18:21:08 +0530

    Refresh without restart using centralized configuration

--- Module-12,13/order-service/src/main/java/com/ecommerce/order_service/controller/OrdersController.java
+++ Module-12,13/order-service/src/main/java/com/ecommerce/order_service/controller/OrdersController.java
@@ -1,10 +1,12 @@
 package com.ecommerce.order_service.controller;
 
+import com.ecommerce.order_service.config.FeaturesEnableConfig;
 import com.ecommerce.order_service.dto.OrderRequestDto;
 import com.ecommerce.order_service.service.OrdersService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +16,21 @@ import java.util.List;
 @RequestMapping("/core")
 @RequiredArgsConstructor
 @Slf4j
+@RefreshScope
 public class OrdersController {
 
     private final OrdersService ordersService;
 
+    private final FeaturesEnableConfig featuresEnableConfig;
+
     @GetMapping("/helloOrders")
     public String helloOrders(@RequestHeader("X-User-Id") Long userId){
-        return "Hello from order service, user id is : "+userId;
+        if (featuresEnableConfig.isUserTrackingEnabled()){
+            return "User tracking enabled, user id is : "+userId;
+        }
+        else {
+            return "User tracking disabled, user id is : "+userId;
+        }
     }
 
     @GetMapping
