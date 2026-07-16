f51dbe10f7108ae1f04a9dd54b7130088620ac19
ajay ratnawat <ajay.ratnawat@synoriq.in>
2026-07-16 20:59:01 +0530

    Kafka User Service

---
+++ Module-14/user-service/src/main/java/com/kafka/user_service/event/UserCreatedEvent.java
@@ -0,0 +1,4 @@
+//package com.kafka.user_service.event;
+//
+//public class UserCreatedEvent {
+//}
---
+++ Module-14/user-service/src/main/java/com/kafka/user_service/user_service/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.kafka.user_service.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+//    @Value("${kafka.topic.user-random-topic}")
+//    private String KAFKA_RANDOM_USER_TOPIC;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+//    private final UserService userService;
+//
+//    @PostMapping
+//    public ResponseEntity<String> createUser(@RequestBody CreateUserRequestDto createUserRequestDto) {
+//        userService.createUser(createUserRequestDto);
+//        return ResponseEntity.ok("User is created");
+//    }
+
+
+//    @PostMapping("/{message}")
+//    public ResponseEntity<String> sendMessage(@PathVariable String message) {
+////        for (int i = 0; i < 1000; i++) {
+////            kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC, ""+i%3, message+i);
+////        }
+//        kafkaTemplate.send("user-random-topic",message);
+//        return ResponseEntity.ok("Message queued");
+//    }
+@PostMapping("/{message}")
+public ResponseEntity<String> sendMessage(@PathVariable String message) {
+    try {
+        kafkaTemplate.send("user-random-topic", message).get();
+        return ResponseEntity.ok("Success");
+    } catch (Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.internalServerError().body(e.toString());
+    }
+}
+
+}
