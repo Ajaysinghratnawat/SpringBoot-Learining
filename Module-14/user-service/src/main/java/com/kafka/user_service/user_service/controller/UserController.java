5d43a29864543f05ee6bda09f166706fca4ed75e
ajay ratnawat <ajay.ratnawat@synoriq.in>
2026-07-17 14:15:22 +0530

    Configuring kafka with springBoot

--- Module-14/user-service/src/main/java/com/kafka/user_service/user_service/controller/UserController.java
+++ Module-14/user-service/src/main/java/com/kafka/user_service/user_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kafka.user_service.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-//    @Value("${kafka.topic.user-random-topic}")
-//    private String KAFKA_RANDOM_USER_TOPIC;
+    @Value("${kafka.topic.user-random-topic}")
+    private String KAFKA_RANDOM_USER_TOPIC;
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 //    private final UserService userService;
@@ -25,23 +26,14 @@ public class UserController {
 //    }
 
 
-//    @PostMapping("/{message}")
-//    public ResponseEntity<String> sendMessage(@PathVariable String message) {
-////        for (int i = 0; i < 1000; i++) {
-////            kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC, ""+i%3, message+i);
-////        }
-//        kafkaTemplate.send("user-random-topic",message);
-//        return ResponseEntity.ok("Message queued");
-//    }
-@PostMapping("/{message}")
-public ResponseEntity<String> sendMessage(@PathVariable String message) {
-    try {
-        kafkaTemplate.send("user-random-topic", message).get();
-        return ResponseEntity.ok("Success");
-    } catch (Exception e) {
-        e.printStackTrace();
-        return ResponseEntity.internalServerError().body(e.toString());
+    @PostMapping("/{message}")
+    public ResponseEntity<String> sendMessage(@PathVariable String message) {
+        for (int i = 0; i < 1000; i++) {
+            kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC, ""+i%3, message+i);
+        }
+        kafkaTemplate.send(KAFKA_RANDOM_USER_TOPIC,message);
+        return ResponseEntity.ok("Message queued");
     }
-}
+
 
 }
